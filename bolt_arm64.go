89e83a30db05a2bbec51ba7d31376ded00f66269
makj <mkj199102022@163.com>
2023-07-05 22:00:46 +0800

    1

---
+++ bolt_arm64.go
@@ -0,0 +1,15 @@
+//go:build arm64
+// +build arm64
+
+package bolt
+
+// maxMapSize represents the largest mmap size supported by Bolt.
+// const maxMapSize = 0xFFFFFFFFFFFF // 256TB
+// const maxMapSize = 1 << 30 // 256TB
+const maxMapSize = 10737418240 // 256TB
+
+// maxAllocSize is the size used when creating array pointers.
+const maxAllocSize = 0x7FFFFFFF
+
+// Are unaligned load/stores broken on this arch?
+var brokenUnaligned = false
